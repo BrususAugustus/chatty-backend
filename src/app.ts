489ea2b0ed254b763ce5989c3359d7cb176c38c8
unknown <drave12345@gmail.com>
2022-12-31 13:50:48 +0100

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Express } from 'express';
+import { ChattyServer } from './setupServer';
+import databaseConnection from './setupDatabase';
+import { config } from './config';
+
+class Application {
+  public initialize(): void {
+    this.loadCconfig();
+    databaseConnection();
+    const app: Express = express();
+    const server: ChattyServer = new ChattyServer(app);
+    server.start();
+  }
+
+  private loadCconfig(): void {
+    config.validateConfig();
+  }
+}
+
+const application: Application = new Application();
+
+application.initialize();
---
+++ src/setupDatabase.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import {config} from "./config"
+import Logger from "bunyan"
+
+const log:  Logger = config.createLogger('setupDatabase')
+
+export default () => {
+  const connect = () => {
+    mongoose
+      .connect(`${config.DATABASE_URL}`)
+      .then(() => {
+        log.info('Connected to the database');
+      })
+      .catch((err) => {
+        log.error('Error connecting to database', err);
+        return process.exit(1);
+      });
+  };
+
+  connect();
+
+  mongoose.connection.on("disconnected", connect)
+};
